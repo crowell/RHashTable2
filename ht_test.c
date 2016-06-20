84465a0539a00201865d9c593c3224c897bb6041
Jeffrey Crowell <crowell@bu.edu>
2016-06-19 23:24:30 -0400

    show better usage in test example

--- ht_test.c
+++ ht_test.c
@@ -1,14 +1,21 @@
 #include <r_util.h>
 #include "ht.h"
 
+void free_kvp (pair_t* kvp) {
+	free (kvp->key);
+	free (kvp->value);
+	free (kvp);
+}
+
 int main (void) {
 	RHashTable2* ht;
-	ht = r_ht_new (r_str_hash64, strcmp, strdup, strdup, NULL);
+	ht = r_ht_new (r_str_hash64, strcmp, strdup, strdup, free_kvp);
 	r_ht_insert (ht, "a", "A");
 	r_ht_insert (ht, "b", "B");
 	r_ht_insert (ht, "c", "C");
 	bool found;
 	char* str = r_ht_find (ht, "a", &found);
 	printf ("%s\n", str);
+	r_ht_destroy (ht);
 	return 0;
 }
