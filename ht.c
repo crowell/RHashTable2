1328d791649d3d2c03f714b818a1ed23fb89569d
Jeffrey Crowell <crowell@bu.edu>
2016-06-19 22:54:19 -0400

    finish code probably

--- ht.c
+++ ht.c
@@ -1,5 +1,145 @@
 #include "ht.h"
 
-int main(void) {
-	printf("%d\n", sizeof(r_ht));
+static RHashTable2* internal_ht_new(ut64 size, RHashFunction hashfunction,
+		RListComparator comparator, RDupKey keydup, RDupValue valdup,
+		RListFree pair_free) {
+	RHashTable2* ht;
+	int i;
+	ht = calloc (1, sizeof(*ht));
+	if (ht != NULL) {
+		ht->size = size;
+		ht->count = 0;
+		ht->table = calloc (ht->size, sizeof (RList*));
+		ht->load_factor = 1;
+		if (ht->table != NULL) {
+			for (i = 0; i < ht->size; ++i) {
+				if (pair_free) {
+					ht->table[i] = r_list_newf (pair_free);
+				} else {
+					ht->table[i] = r_list_new ();
+				}
+			}
+		}
+	}
+	return ht;
 }
+
+RHashTable2* r_ht_new(RHashFunction hashfunction, RListComparator comparator,
+		RDupKey keydup, RDupValue valdup, RListFree pair_free) {
+	ut64 initial_size = 1024;
+	return internal_ht_new (initial_size, hashfunction, comparator, keydup,
+			valdup, pair_free);
+}
+
+void r_ht_destroy(RHashTable2* ht) {
+	int i;
+	for (i = 0; i < ht->size; ++i) {
+		r_list_free (ht->table[i]);
+	}
+	free (ht->table);
+	free (ht);
+}
+
+// Increases the size of the hashtable by 2.
+static void internal_ht_grow(RHashTable2* ht) {
+	RHashTable2* ht2;
+	RHashTable2 swap;
+	pair_t* kvp;
+	RListIter* iter;
+	int i;
+	ht2 = internal_ht_new (ht->size * 2, ht->hashfn, ht->cmp, ht->dupkey,
+			ht->dupvalue, ht->table[0]->free);
+	for (i = 0; i < ht->size; ++i) {
+		r_list_foreach (ht->table[i], iter, kvp) {
+			(void)r_ht_insert (ht2, kvp->key, kvp->value);
+		}
+	}
+	// And now swap the internals.
+	swap = *ht;
+	*ht = *ht2;
+	*ht2 = swap;
+	r_ht_destroy (ht2);
+}
+
+// Inserts the key value pair key, value into the hashtable.
+bool r_ht_insert (RHashTable2* ht, void* key, void* value) {
+	pair_t* kvp;
+	ut64 hash;
+	ut64 bucket;
+	void* blah;
+	if (!r_ht_find (ht, key, blah)) {
+		kvp = calloc (1, sizeof (pair_t *));
+		if (kvp) {
+			if (ht->dupkey) {
+				kvp->key = ht->dupkey (key);
+			} else {
+				kvp->key = key;
+			}
+			if (ht->dupvalue) {
+				kvp->value = ht->dupvalue (value);
+			} else {
+				kvp->value = value;
+			}
+			hash = ht->hashfn (key);
+			bucket = hash % ht->size;
+			r_list_prepend (ht->table[bucket], kvp);
+			ht->count++;
+			// Check if we need to grow the table.
+			if (ht->count >= ht->load_factor * ht->size) {
+				internal_ht_grow (ht);
+			}
+		}
+	}
+	return false;
+}
+
+// Looks up the corresponding value from the key. Returns true if found, false
+// otherwise.
+bool r_ht_find (RHashTable2* ht, void* key, void* value) {
+	ut64 hash;
+	ut64 bucket;
+	RListIter* iter;
+	RList* list;
+	pair_t* kvp;
+	hash = ht->hashfn (key);
+	bucket = hash % ht->size;
+	r_list_foreach (ht->table[bucket], iter, kvp) {
+		if (ht->cmp) {
+			if (ht->cmp (key, kvp->key) == 0) {
+				value = kvp->value;
+				return true;
+			}
+		} else {
+			if (key == kvp->key) {
+				value = kvp->value;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+// Deletes a kvp from the hash table from the key, if the pair exists.
+void r_ht_delete (RHashTable2* ht, void* key) {
+	ut64 hash;
+	ut64 bucket;
+	RListIter* iter;
+	RList* list;
+	pair_t* kvp;
+	hash = ht->hashfn (key);
+	bucket = hash % ht->size;
+	r_list_foreach (ht->table[bucket], iter, kvp) {
+		if (ht->cmp) {
+			if (ht->cmp (key, kvp->key) == 0) {
+				r_list_delete (list, iter);
+				return;
+			}
+		} else {
+			if (key == kvp->key) {
+				r_list_delete (list, iter);
+				return;
+			}
+		}
+	}
+}
+
--- ht.h
+++ ht.h
@@ -19,29 +19,39 @@ typedef ut64 (*RHashFunction)(void* value);
 typedef struct pair_t{
 	void* key;
 	void* value;
-	struct r_ht_entry* next;
 } pair_t;
 
 typedef struct r_ht {
 	ut64 size; // size of the hash table in buckets.
-	RListComparator* cmp; // Function for comparing values. Returns 0 if eq.
+	ut64 count; // number of stored elements.
+	RListComparator cmp; // Function for comparing values. Returns 0 if eq.
 	RHashFunction hashfn; // Function for hashing items in the hash table.
 	RDupKey dupkey; // Function for making a copy of key to store in the entry. NULL for just copying the pointer.
 	RDupValue dupvalue; // Function for making a copy of value to store in the entry. NULL for just copying the pointer.
 	RList/*<pair_t>*/** table;  // Actual table.
+	ut64 load_factor; // load factor before doubling in size.
 } r_ht;
 typedef r_ht RHashTable2;
 
 // Create a new RHashTable2.
 // RHashFunction - the function pointer to the hash function. For a string, use r_str_hash64 for dbj2. Must not be NULL.
-// comparitor - the function pointer to comparison function. Returns 0 for equality eg) for a string strcmp.
+// comparator - the function pointer to comparison function. Returns 0 for equality eg) for a string strcmp.
 // keydup - the function pointer to a dup function. eg) for a string strdup, if NULL is provided, just assignment of the value/pointer
 // valdup - the function pointer to a dup function. eg) for a string strdup, if NULL is provided, just assignment of the value/pointer
 // pair_free - the function pointer to the function to free a pair_t entry, if NULL is provided, just free is used.
-R_API r_ht* r_ht_new(RHashFunction hashfunction, RListComparator comparitor,
+R_API RHashTable2* r_ht_new(RHashFunction hashfunction, RListComparator comparator,
 	RDupKey keydup, RDupValue valdup, RListFree pair_free);
 
 // Destroy a hashtable and all of its entries.
 R_API void r_ht_destroy(RHashTable2* ht);
+
+// Insert a new Key-Value pair into the hashtable. If the key already exists, returns false.
+R_API bool r_ht_insert(RHashTable2* ht, void* key, void* value);
+
+// Delete a key from the hashtable.
+R_API void r_ht_delete(RHashTable2* ht, void* key);
+
+// Find the value corresponding to the matching key.
+R_API bool r_ht_find(RHashTable2* ht, void* key, void* value);
 
 #endif // R_HT_H
