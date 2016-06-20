ef0fe9f4cd4bdf460f0a65de956dcb113ad3932f
Jeffrey Crowell <crowell@bu.edu>
2016-06-20 00:10:23 -0400

    test example where hashes collide

--- ht_test.c
+++ ht_test.c
@@ -10,11 +10,13 @@ void free_kvp (pair_t* kvp) {
 int main (void) {
 	RHashTable2* ht;
 	ht = r_ht_new (r_str_hash64, strcmp, strdup, strdup, free_kvp);
-	r_ht_insert (ht, "a", "A");
-	r_ht_insert (ht, "b", "B");
+	r_ht_insert (ht, "bc", "A");
+	r_ht_insert (ht, "cB", "B");
 	r_ht_insert (ht, "c", "C");
 	bool found;
-	char* str = r_ht_find (ht, "a", &found);
+	char* str = r_ht_find (ht, "bc", &found);
+	printf ("%s\n", str);
+	str = r_ht_find (ht, "cB", &found);
 	printf ("%s\n", str);
 	r_ht_destroy (ht);
 	return 0;
